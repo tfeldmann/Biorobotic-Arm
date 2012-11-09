5ff2b32bbb0e33e954fe072ba683ce64f41aa762
Thomas Feldmann <feldmann.thomas@googlemail.com>
2012-11-09 18:44:34 +0100

    tab->spaces

--- Firmware/BioArm_Firmware/SerialCmd.ino
+++ Firmware/BioArm_Firmware/SerialCmd.ino
@@ -3,8 +3,8 @@ SerialCommand serialCommand;
 
 void scmd_init()
 {
-	serialCommand.addCommand("?",        scmd_identify);
-	serialCommand.addCommand("BASE",     scmd_base);
+    serialCommand.addCommand("?",        scmd_identify);
+    serialCommand.addCommand("BASE",     scmd_base);
     serialCommand.addCommand("SHOULDER", scmd_shoulder);
     serialCommand.addCommand("WRIST",    scmd_wrist);
     serialCommand.addCommand("GRIP",     scmd_grip);
@@ -24,22 +24,22 @@ void scmd_unknown(const char *command)
 
 void scmd_identify()
 {
-	Serial.println("BIOROBOTIC_ARM_1.0");
+    Serial.println("BIOROBOTIC_ARM_1.0");
 }
 
 void scmd_base()
 {
-	Serial.println("# BASE [ANGLE]");
+    Serial.println("# BASE [ANGLE]");
 }
 
 void scmd_shoulder()
 {
-	Serial.println("# SHOULDER [ANGLE]");
+    Serial.println("# SHOULDER [ANGLE]");
 }
 
 void scmd_wrist()
 {
-	Serial.println("# WRIST [ANGLE]");
+    Serial.println("# WRIST [ANGLE]");
 }
 
 void scmd_grip()
