7a28a0d7017580c26827db92d0a3f519e38d199e
Thomas Feldmann <feldmann.thomas@googlemail.com>
2012-12-17 15:46:33 +0100

    Gone back to old collision code

--- Firmware/Collision.ino
+++ Firmware/Collision.ino
@@ -1,20 +1,51 @@
-/*
-    MMA8452Q Example Code
-    by: Jim Wright
-    Based on original example code by: Jim Lindblom, SparkFun Electronics
-    date: March 28, 2012
-    license: Beerware - Use this code however you'd like. If you
-    find it useful you can buy me a beer some time.
+/* MMA8452Q Example Code
+by: Jim Wright
+Based on original example code by: Jim Lindblom, SparkFun Electronics
+date: March 28, 2012
+license: Beerware - Use this code however you'd like. If you
+find it useful you can buy me a beer some time.
 
-    Hardware setup:
-    MMA8452 Breakout ------------ Arduino
-       3.3V --------------------- 3.3V
-       SDA ----------------------- 20
-       SCL ----------------------- 21
-       GND ---------------------- GND
+This example code is essentially the same as the original code written
+by Jim Lindblom, with the exception that the standard Arduino Wire library
+is used instead of the custom i2c library originally supplied.
+At the time of this writing, the release version of the Arduino Wire
+library does not support repeated start commands, which are required by this
+chip.  A revised Wire library with the necessary patch applied was committed
+on Feb 18, 2012.  For this code to work, you will need to replace your Wire
+library with the version at: https://github.com/arduino/Arduino/tree/master/libraries/Wire
 
-    SDA and SCL should have external pull-up resistors (to 3.3V). They should be on
-    the breakout board.
+Presumably, Arduino software after v1.0 should have this new code built in.
+
+
+This code should provide example usage for most features of
+the MMA8452Q 3-axis, I2C accelerometer. In the loop function
+the accelerometer interrupt outputs will be polled, and either
+the x/y/z accel data will be output, or single/double-taps,
+portrait/landscape changes will be announced to the serial port.
+Feel free to comment/uncomment out some of the Serial.print
+lines so you can see the information you're most intereseted in.
+
+The skeleton is here, feel free to cut/paste what code you need.
+Play around with the settings in initMMA8452Q. Try running the
+code without printing the accel values, to really appreciate
+the single/double-tap and portrait landscape functions. The
+P/L stuff is really neat, something not many accelerometers have.
+
+Hardware setup:
+MMA8452 Breakout ------------ Arduino
+   3.3V --------------------- 3.3V
+   SDA ----------------------- A4
+   SCL ----------------------- A5
+   INT2 ---------------------- D3
+   INT1 ---------------------- D2
+   GND ---------------------- GND
+
+SDA and SCL should have external pull-up resistors (to 3.3V).
+10k resistors worked for me. They should be on the breakout
+board.
+
+Arduino Mega boards have SDA on pin 20, and SCL on pin 21.  Please wire your board accordin
+
 */
 #include <Wire.h>
 
@@ -31,6 +62,10 @@ const byte SCALE = 4;  // Sets full-scale range to +/-2, 4, or 8g. Used to calc 
 /* Set the output data rate below. Value should be between 0 and 7*/
 const byte dataRate = 0;  // 0=800Hz, 1=400, 2=200, 3=100, 4=50, 5=12.5, 6=6.25, 7=1.56
 
+/* Pin definitions */
+int int1Pin = 2;  // These can be changed, 2 and 3 are the Arduinos ext int pins
+int int2Pin = 3;
+
 int accelCount[3];  // Stores the 12-bit signed value
 float accelG[3];  // Stores the real accel value in g's
 
@@ -43,11 +78,11 @@ void collision_init()
     if (c == 0x2A) // WHO_AM_I should always be 0x2A
     {
         initMMA8452(SCALE, dataRate);  // init the accelerometer if communication is OK
-        Serial.println("# Accelerometer found");
+        Serial.println("MMA8452Q is online...");
     }
     else
     {
-        Serial.print("!Could not connect to MMA8452Q: 0x");
+        Serial.print("Could not connect to MMA8452Q: 0x");
         Serial.println(c, HEX);
         while (1);  // Loop forever if communication doesn't happen
     }
@@ -118,49 +153,78 @@ void tapHandler()
 {
     byte source = readRegister(0x22);  // Reads the PULSE_SRC register
 
-    // if ((source & 0x10)==0x10)  // If AxX bit is set
-    // {
-    //     if ((source & 0x08)==0x08)  // If DPE (double puls) bit is set
-    //         Serial.print("    Double Tap (2) on X");  // tabbing here for visibility
-    //     else
-    //         Serial.print("Single (1) tap on X");
+    if ((source & 0x10)==0x10)  // If AxX bit is set
+    {
+        if ((source & 0x08)==0x08)  // If DPE (double puls) bit is set
+            Serial.print("    Double Tap (2) on X");  // tabbing here for visibility
+        else
+            Serial.print("Single (1) tap on X");
 
-    //     if ((source & 0x01)==0x01)  // If PoIX is set
-    //         Serial.println(" +");
-    //     else
-    //         Serial.println(" -");
-    // }
+        if ((source & 0x01)==0x01)  // If PoIX is set
+            Serial.println(" +");
+        else
+            Serial.println(" -");
+    }
 
-    // if ((source & 0x20)==0x20)  // If AxY bit is set
-    // {
-    //     if ((source & 0x08)==0x08)  // If DPE (double puls) bit is set
-    //         Serial.print("    Double Tap (2) on Y");
-    //     else
-    //         Serial.print("Single (1) tap on Y");
+    if ((source & 0x20)==0x20)  // If AxY bit is set
+    {
+        if ((source & 0x08)==0x08)  // If DPE (double puls) bit is set
+            Serial.print("    Double Tap (2) on Y");
+        else
+            Serial.print("Single (1) tap on Y");
 
-    //     if ((source & 0x02)==0x02)  // If PoIY is set
-    //         Serial.println(" +");
-    //     else
-    //         Serial.println(" -");
-    // }
+        if ((source & 0x02)==0x02)  // If PoIY is set
+            Serial.println(" +");
+        else
+            Serial.println(" -");
+    }
 
-    // if ((source & 0x40)==0x40)  // If AxZ bit is set
-    // {
-    //     if ((source & 0x08)==0x08)  // If DPE (double puls) bit is set
-    //         Serial.print("    Double Tap (2) on Z");
-    //     else
-    //         Serial.print("Single (1) tap on Z");
-    //     if ((source & 0x04)==0x04)  // If PoIZ is set
-    //         Serial.println(" +");
-    //     else
-    //         Serial.println(" -");
-    // }
+    if ((source & 0x40)==0x40)  // If AxZ bit is set
+    {
+        if ((source & 0x08)==0x08)  // If DPE (double puls) bit is set
+            Serial.print("    Double Tap (2) on Z");
+        else
+            Serial.print("Single (1) tap on Z");
+        if ((source & 0x04)==0x04)  // If PoIZ is set
+            Serial.println(" +");
+        else
+            Serial.println(" -");
+    }
 
     if ((source & 0x40) == 0x40 || (source & 0x20) == 0x20 || (source & 0x10) == 0x10)
     {
         stop();
         tone_collision();
     }
+}
+
+/* This function will read the p/l source register and
+print what direction the sensor is now facing */
+void portraitLandscapeHandler()
+{
+byte pl = readRegister(0x10);  // Reads the PL_STATUS register
+switch((pl&0x06)>>1)  // Check on the LAPO[1:0] bits
+{
+case 0:
+  Serial.print("Portrait up, ");
+  break;
+case 1:
+  Serial.print("Portrait Down, ");
+  break;
+case 2:
+  Serial.print("Landscape Right, ");
+  break;
+case 3:
+  Serial.print("Landscape Left, ");
+  break;
+}
+if (pl&0x01)  // Check the BAFRO bit
+Serial.print("Back");
+else
+Serial.print("Front");
+if (pl&0x40)  // Check the LO bit
+Serial.print(", Z-tilt!");
+Serial.println();
 }
 
 /* Initialize the MMA8452 registers
@@ -176,20 +240,20 @@ void initMMA8452(byte fsr, byte dataRate)
 
     /* Set up the full scale range to 2, 4, or 8g. */
     if ((fsr==2)||(fsr==4)||(fsr==8))
-        writeRegister(0x0E, fsr >> 2);
+    writeRegister(0x0E, fsr >> 2);
     else
-        writeRegister(0x0E, 0);
+    writeRegister(0x0E, 0);
 
     /* Setup the 3 data rate bits, from 0 to 7 */
     writeRegister(0x2A, readRegister(0x2A) & ~(0x38));
     if (dataRate <= 7)
-        writeRegister(0x2A, readRegister(0x2A) | (dataRate << 3));
+    writeRegister(0x2A, readRegister(0x2A) | (dataRate << 3));
 
     /* Set up portrait/landscap registers - 4 steps:
      1. Enable P/L
      2. Set the back/front angle trigger points (z-lock)
      3. Set the threshold/hysteresis angle
-     4. Set the debounce rate
+     4. Set the debouce rate
     // For more info check out this app note: http://cache.freescale.com/files/sensors/doc/app_note/AN4068.pdf */
     writeRegister(0x11, 0x40);  // 1. Enable P/L
     writeRegister(0x13, 0x44);  // 2. 29deg z-lock (don't think this register is actually writable)
@@ -203,17 +267,22 @@ void initMMA8452(byte fsr, byte dataRate)
      4. Set the pulse latency - the minimum required time between one pulse and the next
      5. Set the second pulse window - maximum allowed time between end of latency and start of second pulse
      for more info check out this app note: http://cache.freescale.com/files/sensors/doc/app_note/AN4072.pdf */
-    writeRegister(0x21, 0x7F);  // 1. enable single/double taps on all axes
-    // writeRegister(0x21, 0x55);  // 1. single taps only on all axes
+    // writeRegister(0x21, 0x7F);  // 1. enable single/double taps on all axes
+    writeRegister(0x21, 0x55);  // 1. single taps only on all axes
     // writeRegister(0x21, 0x6A);  // 1. double taps only on all axes
 
-    writeRegister(0x23, 0x16);  // 2. x thresh at 2g, multiply the value by 0.0625g/LSB to get the threshold
-    writeRegister(0x24, 0x16);  // 2. y thresh at 2g, multiply the value by 0.0625g/LSB to get the threshold
+    writeRegister(0x23, 0x10);  // 2. x thresh at 2g, multiply the value by 0.0625g/LSB to get the threshold
+    writeRegister(0x24, 0x10);  // 2. y thresh at 2g, multiply the value by 0.0625g/LSB to get the threshold
     writeRegister(0x25, 0x16);  // 2. z thresh at .5g, multiply the value by 0.0625g/LSB to get the threshold
 
     writeRegister(0x26, 0x30);  // 3. 30ms time limit at 800Hz odr, this is very dependent on data rate, see the app note
     writeRegister(0x27, 0xA0);  // 4. 200ms (at 800Hz odr) between taps min, this also depends on the data rate
     writeRegister(0x28, 0xFF);  // 5. 318ms (max value) between taps max
+
+    /* Set up interrupt 1 and 2 */
+    writeRegister(0x2C, 0x02);  // Active high, push-pull interrupts
+    writeRegister(0x2D, 0x19);  // DRDY, P/L and tap ints enabled
+    writeRegister(0x2E, 0x01);  // DRDY on INT1, P/L and taps on INT2
 
     MMA8452Active();  // Set to active to start reading
 }
