52234351a535589ed513594b7c37c40348736b04
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-03-15 23:19:35 +0100

    added log widget and enhanced gui

--- FaceRecognition/videowidget.py
+++ FaceRecognition/videowidget.py
@@ -26,11 +26,15 @@ class VideoWidget(QWidget):
         # set refresh rate
         self.timer = QTimer(self)
         self.timer.timeout.connect(self._query_frame)
-        self.timer.start(50)
+        self.timer.start(75)
 
 
     def _build_image(self, image):
-        cv.Resize(image, self.frame, cv.CV_INTER_NN)
+        # resize the image if it does not fit into the application
+        if image.height == 480 and image.width == 640:
+            self.frame = image
+        else:
+            cv.Resize(image, self.frame, cv.CV_INTER_NN)
         return OpenCVQImage(self.frame)
 
 
@@ -45,39 +49,19 @@ class VideoWidget(QWidget):
         painter.drawImage(QPoint(0, 0), self.qimage)
 
 
-
-class IplQImage(QImage):
-    """
-    http://matthewshotton.wordpress.com/2011/03/31/python-opencv-iplimage-to-pyqt-qimage/
-    A class for converting iplimages to qimages
-    """
-    def __init__(self,iplimage):
-        # Rough-n-ready but it works dammit
-        alpha = cv.CreateMat(iplimage.height,iplimage.width, cv.CV_8UC1)
-        cv.Rectangle(alpha, (0, 0), (iplimage.width,iplimage.height), cv.ScalarAll(255) ,-1)
-        rgba = cv.CreateMat(iplimage.height, iplimage.width, cv.CV_8UC4)
-        cv.Set(rgba, (1, 2, 3, 4))
-        cv.MixChannels([iplimage, alpha],[rgba], [
-        (0, 0), # rgba[0] -> bgr[2]
-        (1, 1), # rgba[1] -> bgr[1]
-        (2, 2), # rgba[2] -> bgr[0]
-        (3, 3)  # rgba[3] -> alpha[0]
-        ])
-        self.__imagedata = rgba.tostring()
-        super(IplQImage,self).__init__(self.__imagedata, iplimage.width, iplimage.height, QImage.Format_RGB32)
-
-
-
 class OpenCVQImage(QImage):
     def __init__(self, opencvBgrImg):
         depth, nChannels = opencvBgrImg.depth, opencvBgrImg.nChannels
         if depth != cv.IPL_DEPTH_8U or nChannels != 3:
             raise ValueError("the input image must be 8-bit, 3-channel")
+
         w, h = cv.GetSize(opencvBgrImg)
         opencvRgbImg = cv.CreateImage((w, h), depth, nChannels)
+
         # it's assumed the image is in BGR format
         cv.CvtColor(opencvBgrImg, opencvRgbImg, cv.CV_BGR2RGB)
         self._imgData = opencvRgbImg.tostring()
+
         super(OpenCVQImage, self).__init__(self._imgData, w, h, \
             QImage.Format_RGB888)
 
