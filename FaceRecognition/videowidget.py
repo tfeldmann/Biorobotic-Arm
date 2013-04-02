fb56d12953b36a5960eca84706fec1b4face273e
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-04-02 14:01:18 +0200

    added face recognition

--- FaceRecognition/videowidget.py
+++ FaceRecognition/videowidget.py
@@ -3,10 +3,8 @@ import cv2.cv as cv
 from PySide.QtCore import QPoint, QTimer
 from PySide.QtGui import QWidget, QPainter, QImage, QApplication
 
-
-CASCADE_PATH = "/usr/local/Cellar/opencv/2.4.3/share/OpenCV/haarcascades/"
-FRONTALFACE_PATH = CASCADE_PATH+"haarcascade_frontalface_alt.xml"
-PROFILEFACE_PATH = CASCADE_PATH+"haarcascade_profileface.xml"
+FRONTALFACE_PATH = "haarcascades/haarcascade_frontalface_alt.xml"
+PROFILEFACE_PATH = "haarcascades/haarcascade_profileface.xml"
 CAMERA_INDEX = 0
 
 
@@ -15,10 +13,10 @@ class VideoWidget(QWidget):
 
     def __init__(self, parent=None):
         QWidget.__init__(self)
-
-        # set widget dimensions
         self.setMinimumSize(640, 480)
         self.setMaximumSize(self.minimumSize())
+
+        self.face_callback = None
 
         # init view with correct size, depth, channels
         self.frame = cv.CreateImage((640,480), cv.IPL_DEPTH_8U, 3)
@@ -37,21 +35,27 @@ class VideoWidget(QWidget):
         self.timer.timeout.connect(self._query_frame)
         self.timer.start(75)
 
-
     def _build_image(self, image):
-        # resize the image if it does not fit into the application
+        """ resize the image if it does not fit into the application """
         if image.height == 480 and image.width == 640:
             self.frame = image
         else:
             cv.Resize(image, self.frame, cv.CV_INTER_NN)
         return OpenCVQImage(self.frame)
 
-
     def _query_frame(self):
         image = cv.QueryFrame(self.capture)
+        faces = self._detect_faces(image)
+        for (x,y,w,h) in faces:
+            cv.Rectangle(image, (x, y), (x+w, y+h), cv.RGB(0, 255, 0), 2)
+
         self.qimage = self._build_image(image)
         self.update()
 
+    def _detect_faces(self, image):
+        detected = cv.HaarDetectObjects(image, self.frontalface_cascade,
+            self.storage, 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING, (100,100))
+        return [(x,y,w,h) for (x,y,w,h),n in detected]
 
     def paintEvent(self, event):
         painter = QPainter(self)
